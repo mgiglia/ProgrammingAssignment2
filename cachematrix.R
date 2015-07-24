9060f87edbb494c0235d3750b68ddd9dcd465283
mgiglia <matthewgiglia@mac.com>
2015-07-24 12:20:56 -0400

    Final submission for peer review.

--- cachematrix.R
+++ cachematrix.R
@@ -1,5 +1,12 @@
 ## Matrix inversion is usually a costly computation and there may be some benefits
 ## to caching the inverse of a matrix rather than compute it repeatedly.
+## The funcitons below work together to allow us to use caching for inverse calculations
+## by first creating a special matrix object that is a list that contains the original
+## matrix plus positions to set or return the inverse of the matrix if it's already been
+## solved. The special matrix is then used in the second function and checks to see
+## if the matrix's inverse is solved or not already.  If it is solved, then it returns 
+## the inverse from the cached data.  If it hasn't been solved, then the second function
+## finds the inverse and then store's it in the "matrix" object.
 
 ## makeCacheMatrix: This function creates a special "matrix" object that can cache its inverse.
 
@@ -34,7 +41,10 @@ cacheSolve <- function(x, ...) {
       ## The identity matrix is a square matrix with 1's down the diagonal
       ## from the top left to the bottom right and zeros everywhere else.
       ## This can be attained by using the diag function.
-      m <- solve(data, diag(nrow(data)), ...)
+      b <- diag(nrow(data))
+      m <- solve(data, b, ...)
       x$setinverse(m)
       m
 }
+
+## M.Giglia 7/24/2015 for Coursera R Programming Assignment 2
